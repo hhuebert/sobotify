1dcbf7a4e9fe8def72bc91c4294b91b5fc064dbd
Heiko Huebert <heiko.huebert@htw-berlin.de>
2023-05-18 02:20:49 +0200

    added --no-capture-output to bat-scripts

--- scripts/play_pepper.bat
+++ scripts/play_pepper.bat
@@ -1,4 +1,4 @@
 set ROBOT_IP=192.168.0.141
 set LANGUAGE=english
 set MESSAGE="%~n1|Hello"
-CALL "%USERPROFILE%\miniconda3\condabin\conda.bat" run -n sobotify_naoqi python "%~dp0\..\sobotify\robotcontrol\robotcontrol.py" --robot_name pepper --robot_ip %ROBOT_IP% --language %LANGUAGE% --message %MESSAGE%
+CALL "%USERPROFILE%\miniconda3\condabin\conda.bat" run -n sobotify_naoqi --no-capture-output python "%~dp0\..\sobotify\robotcontrol\robotcontrol.py" --robot_name pepper --robot_ip %ROBOT_IP% --language %LANGUAGE% --message %MESSAGE%
--- scripts/play_stickman.bat
+++ scripts/play_stickman.bat
@@ -1,4 +1,4 @@
 set LANGUAGE=english
 set MESSAGE="%~n1|Hello"
-CALL "%USERPROFILE%\miniconda3\condabin\conda.bat" run -n sobotify python "%~dp0\..\sobotify\robotcontrol\robotcontrol.py" --robot_name stickman --language %LANGUAGE% --message %MESSAGE%
+CALL "%USERPROFILE%\miniconda3\condabin\conda.bat" run -n sobotify --no-capture-output python "%~dp0\..\sobotify\robotcontrol\robotcontrol.py" --robot_name stickman --language %LANGUAGE% --message %MESSAGE%
 PAUSE
