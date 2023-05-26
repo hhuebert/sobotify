67ddc2468b1bdf960c988d857adc487f56341d4f
Heiko Huebert <heiko.huebert@htw-berlin.de>
2023-05-26 19:33:57 +0200

    add play_nao, add PAUSE+ROBOT_NAME  to play_pepper

--- examples/play_nao.bat
+++ examples/play_nao.bat
@@ -1,5 +1,5 @@
 set ROBOT_IP=192.168.0.141
-set ROBOT_NAME=pepper
+set ROBOT_NAME=nao
 set LANGUAGE=english
 set MESSAGE="%~n1|Hello"
 CALL "%USERPROFILE%\miniconda3\condabin\conda.bat" run -n sobotify_naoqi --no-capture-output python "%~dp0\..\sobotify\robotcontrol\robotcontrol.py" --robot_name %ROBOT_NAME% --robot_ip %ROBOT_IP% --language %LANGUAGE% --message %MESSAGE%
