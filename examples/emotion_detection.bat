88cabbae67ab4293b55d5fc391f747040a32e9c4
Heiko Huebert <heiko.huebert@htw-berlin.de>
2023-06-18 01:11:30 +0200

    add emotion detection batch file

---
+++ examples/emotion_detection.bat
@@ -0,0 +1,17 @@
+set ROBOT_NAME=stickman
+@ECHO OFF
+REM ======================================================================================
+REM find and set CONDA executable path
+if EXIST "%USERPROFILE%\miniconda3\condabin\conda.bat" (
+    set CONDA="%USERPROFILE%\miniconda3\condabin\conda.bat" 
+) else if EXIST "%USERPROFILE%\AppData\Local\miniconda3\condabin\conda.bat" (
+    set CONDA="%USERPROFILE%\AppData\Local\miniconda3\condabin\conda.bat" 
+) else (
+    echo cannot find CONDA environment. Installation aborted!
+    pause
+    exit
+)
+REM ======================================================================================
+@ECHO ON
+CALL %CONDA% run -n sobotify --no-capture-output python "%~dp0\..\sobotify\sobotify.py" -f --robot_name=%ROBOT_NAME%
+PAUSE
